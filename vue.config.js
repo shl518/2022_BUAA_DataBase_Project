2b2118ea7bda799f26b684afbd01636194d6e9e7
shl518 <71417331+shl518@users.noreply.github.com>
2022-09-23 14:30:39 +0800

    atest

--- vue.config.js
+++ vue.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
     proxy :{
       "/dev-api": {
-        target: "http://localhost:3000", // 接口地址
+        target: "http://127.0.0.1:3000", // 接口地址
         pathRewrite: {"^/dev-api" : ""} // 路径重写
       }
     }
