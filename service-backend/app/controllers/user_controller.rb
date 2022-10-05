056f6d23462f7d7b6999819042e424d37436d785
coolcoloury <coolcoloury@icloud.com>
2022-10-05 16:15:37 +0800

    Changes

--- service-backend/app/controllers/user_controller.rb
+++ service-backend/app/controllers/user_controller.rb
@@ -31,8 +31,8 @@ class UserController < ApplicationController
     # 以下为使用sql语句进行插入操作
     begin
       execute_statement("INSERT INTO users(id, name, password, created_at, updated_at)
-      VALUES (#{id}, #{user[:name]}, #{user[:password]}, #{DateTime.parse(Time.now.to_s).strftime('%Y-%m-%d %H:%M:%S')},
-        #{DateTime.parse(Time.now.to_s).strftime('%Y-%m-%d %H:%M:%S')});")
+      VALUES (#{id}, '#{user[:name]}', '#{user[:password]}', '#{DateTime.parse(Time.now.to_s).strftime('%Y-%m-%d %H:%M:%S')}',
+        '#{DateTime.parse(Time.now.to_s).strftime('%Y-%m-%d %H:%M:%S')}');")
 
       render status: 200, json: response_json(
         true,
