70a857bf26f1ebd40692df013e844ccb3dd08516
coolcoloury <coolcoloury@icloud.com>
2022-10-12 21:14:13 +0800

    Changes

--- service-backend/app/controllers/dish_controller.rb
+++ service-backend/app/controllers/dish_controller.rb
@@ -30,7 +30,7 @@ class DishController < ApplicationController
     begin
       execute_statement("INSERT INTO dishes(id, name, price, star, created_at, updated_at)
       VALUES(#{@@id}, '#{dish[:name]}', #{dish[:price]} , '#{dish[:star]}','#{DateTime.parse(Time.now.to_s).strftime('%Y-%m-%d %H:%M:%S')}',
-       '#{DateTime.parse(Time.now.to_s).strftime('%Y-%m-%d %H:%M:%S')}'")
+       '#{DateTime.parse(Time.now.to_s).strftime('%Y-%m-%d %H:%M:%S')}');")
 
       render status: 200, json: response_json(
         true,
